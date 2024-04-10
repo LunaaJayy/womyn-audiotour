d9e597024abdf70ad7a2b29fc24fef2a5b7aae5e
LunaaJayy <152217714+LunaaJayy@users.noreply.github.com>
2024-04-10 14:13:44 +0200

    website af zonder fotos en audio!

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const startknop = document.querySelector("button");
+const titel = document.querySelector("p");
+const foto = document.querySelector(".defotos");
+const pijlLinks = document.querySelector(".pijl-links");
+const pijlRechts = document.querySelector(".pijl-rechts");
+const audioElement = new Audio();
+const afspeelButton = document.querySelector(".playbutton")
 
 let startscherm = document.querySelector(".startscherm");
 let dagscherm = document.querySelector(".dag");
@@ -6,11 +12,58 @@ let fotoArray = [
     "images/test.png",
     "images/test2.jpg"
 ];
+let titelArray = [
+    "Dag 1", "Dag 2", "Dag 3", "Dag 4", "Dag 5", "Dag 6", 
+    "Dag 7", "Dag 8", "Dag 9", "Dag 10", "Dag 11", "Dag 12", 
+    "Dag 13", "Dag 14", "Dag 15", "Dag 16", "Dag 17", "Dag 18", 
+    "Dag 19", "Dag 20", "Dag 21", "Dag 22", "Dag 23", "Dag 24",
+    "Dag 25", "Dag 26", "Dag 27", "Dag 28"
+];
+
+let audioArray = [
+    "audio/test.mp3", "audio/test2.mp3"
+];
+
+let huidigeIndex = 0
+
 
 function starten () {
     startscherm.classList.add("hidden");
     dagscherm.classList.remove("hidden");
+    toonFotoEnTitel();
+}
+
+function toonFotoEnTitel() {
+    const fotoSRC = fotoArray[huidigeIndex];
+    const dagTitel = titelArray[huidigeIndex];
+    const audioSRC = audioArray[huidigeIndex];
+
+    foto.src = fotoSRC;
+    titel.textContent = dagTitel;
+    audioElement.src = audioSRC;
+}
+
+function naarLinks() {
+    if (huidigeIndex > 0) {
+        huidigeIndex--;
+        toonFotoEnTitel();
+    }
+}
+
+function naarRechts() {
+    if (huidigeIndex < fotoArray.length - 1){
+        huidigeIndex++;
+        toonFotoEnTitel();
+    }
+}
+
+function speelAudio() {
+    audioElement.play()
 }
 
 startknop.addEventListener('click', starten)
+pijlLinks.addEventListener('click', naarLinks)
+pijlRechts.addEventListener('click', naarRechts)
+afspeelButton.addEventListener('click', speelAudio)
+
 
