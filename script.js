54122f57b94064071306ae70bb4650fced94d370
LunaaJayy <152217714+LunaaJayy@users.noreply.github.com>
2024-04-17 16:35:14 +0200

    echt af!

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ let fotoArray = [
     "dagen/lente.png", "dagen/dag7.png", "dagen/dag8.png", "dagen/dag9.png", "dagen/dag10.png", "dagen/dag11.png", "dagen/dag12.png", 
     "dagen/zomer.png", "dagen/dag13.png", "dagen/dag14.png", "dagen/dag15.png", "dagen/dag16.png", "dagen/dag17.png", "dagen/dag18.png", 
     "dagen/dag19.png", "dagen/dag20.png", "dagen/dag25.png", "dagen/dag21.png", "dagen/dag22.png", "dagen/dag23.png", "dagen/dag24.png",
-    "dagen/dag25.png", "dagen/dag26.png", "dagen/dag27.png", "dagen/dag28.png", "images/vierkant.png"
+    "dagen/dag25.png", "dagen/dag26.png", "dagen/dag27.png", "dagen/dag28.png", "images/vierkant.png", "images/vierkant.png"
 ];
 let titelArray = [
     "Welkom", "Winter", "Dag 1", "Dag 2", "Dag 3", "Dag 4", "Dag 5", "Dag 6", 
     "Lente", "Dag 7", "Dag 8", "Dag 9", "Dag 10", "Dag 11", "Dag 12", 
     "Zomer", "Dag 13", "Dag 14", "Dag 15", "Dag 16", "Dag 17", "Dag 18", 
     "Dag 19", "Dag 20", "Herfst", "Dag 21", "Dag 22", "Dag 23", "Dag 24",
-    "Dag 25", "Dag 26", "Dag 27", "Dag 28", "Als laatste.."
+    "Dag 25", "Dag 26", "Dag 27", "Dag 28", "Als laatste..",""
 ];
 
 let audioArray = [
@@ -28,7 +28,7 @@ let audioArray = [
     "audio/audiolente.mp3", "audio/audiodag7.mp3", "audio/audiodag8.mp3", "audio/audiodag9.mp3", "audio/audiodag10.mp3", "audio/audiodag11.mp3", "audio/audiodag12.mp3", 
     "audio/audiozomer.mp3", "audio/audiodag13.mp3", "audio/audiodag14.mp3", "audio/audiodag15.mp3", "audio/audiodag16.mp3", "audio/audiodag17.mp3", "audio/audiodag18.mp3", 
     "audio/audiodag19.mp3", "audio/audiodag20.mp3", "audio/audioherfst.mp3", "audio/audiodag21.mp3", "audio/audiodag22.mp3", "audio/audiodag23.mp3", "audio/audiodag24.mp3", 
-    "audio/audiodag25.mp3", "audio/audiodag26.mp3", "audio/audiodag27.mp3", "audio/audiodag28.mp3", "audio/audiooutro.mp3"
+    "audio/audiodag25.mp3", "audio/audiodag26.mp3", "audio/audiodag27.mp3", "audio/audiodag28.mp3", "audio/audiooutro.mp3", "audio/audiooutro.mp3"
 ];
 
 let huidigeIndex = 0
@@ -52,7 +52,6 @@ function toonFotoEnTitel() {
 
     if ( dagTitel === "Welkom") {
         pijlLinks.src = "images/pijl-wit.png"
-        console.log("ghello?")
     }
     else {
         pijlLinks.src = "images/pijltje-links.png"
@@ -74,7 +73,7 @@ function naarRechts() {
         huidigeIndex++;
         toonFotoEnTitel();
     
-        if (titelArray[huidigeIndex] === "Als laatste..") {
+        if (titelArray[huidigeIndex] === "") {
             location.reload();
         }
     }
